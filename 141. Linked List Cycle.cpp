61d3fce3bbcfd165c975d40b3d942c67e36b2f48
thekingofcity <wzl7777777@gmail.com>
2019-02-25 16:33:05 +0800

    141,142

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -27,3 +27,16 @@ public:
         }
     }
 };
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL) return false;
+        ListNode *slow=head,*fast=head;
+        while(fast->next!=NULL&&fast->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+};
