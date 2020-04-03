e4db83a2d9ef90ec977efb269c886aae61ab5da0
thekingofcity <wzl7777777@gmail.com>
2020-04-03 14:20:37 +0800

    3 revisit

--- 3.Longest Substring Without Repeating Characters.cpp
+++ 3.Longest Substring Without Repeating Characters.cpp
@@ -19,3 +19,13 @@ public:
     }
 };
 //Runtime: 73 ms
+int lengthOfLongestSubstring(string s) {
+        vector<int> map(128,0);
+        int counter=0, begin=0, end=0, d=0; 
+        while(end<s.size()){
+            if(map[s[end++]]++>0) counter++; 
+            while(counter>0) if(map[s[begin++]]-->1) counter--;
+            d=max(d, end-begin); //while valid, update d
+        }
+        return d;
+    }
