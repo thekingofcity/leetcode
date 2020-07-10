349172b407eaba97ede7f929204fd6d792bf730b
thekingofcity <wzl7777777@gmail.com>
2020-07-10 21:42:12 +0800

    92 revisit

--- 92. Reverse Linked List II.cpp
+++ 92. Reverse Linked List II.cpp
@@ -42,6 +42,60 @@ public:
         return dummyHead->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        if(!head||m==n) return head;
+        bool first=false;
+        if(m==1){
+            first=true;
+            head=new ListNode(-1, head);
+            ++m; ++n;
+        }
+        
+        ListNode *pre,*cur=head,*leftEnd=head,*revEnd,*tmp;
+        int c=1;
+        while(cur){
+            if(c==m){
+                revEnd=cur;
+                leftEnd=pre;
+                pre=cur;
+                tmp=cur->next;
+                cur->next=NULL;
+                cur=tmp;
+            }else if(c>m&&c<=n){
+                tmp=cur->next;
+                cur->next=pre;
+                pre=cur;
+                cur=tmp;
+            }else if(c>n){
+                revEnd->next=cur;
+                leftEnd->next=pre;
+                break;
+            }else{
+                pre=cur;
+                cur=cur->next;
+            }
+            ++c;
+        }
+        if(c==n+1) leftEnd->next=pre;
+        
+        if(first) return head->next;
+        return head;
+    }
+};
+
 /*
 [1,2,3,4,5]
 2
@@ -50,18 +104,12 @@ public:
 1
 4
 [1,2,3,4,5]
+1
+5
+[1,2,3,4,5]
 2
 5
 [1,2,3,4,5]
-1
-1
-[1,2,3,4,5]
-5
-5
-[1,2,3,4,5]
 3
 3
-[1,2,3,4,5]
-5
-5
 */
