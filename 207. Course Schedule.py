cbae07b41c102feac60f8d13c4a96983acb8dba3
thekingofcity <wzl7777777@gmail.com>
2020-07-26 17:30:14 +0800

    207 beautify

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,21 +1,22 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prerequisite={}
-        for a,b in prerequisites:
+    def canFinish(self, numCourses: int,
+                  prerequisites: List[List[int]]) -> bool:
+        prerequisite = {}
+        for a, b in prerequisites:
             prerequisite.setdefault(a, []).append(b)
 
-        c=[not i in prerequisite for i in range(numCourses)]
+        c = [i not in prerequisite for i in range(numCourses)]
 
-        flag=True
+        flag = True
         while flag:
-            flag=False
-            used_k=[]  # do not modify dict on fly
-            for k,v in prerequisite.items():
+            flag = False
+            used_k = []  # do not modify dict on fly
+            for k, v in prerequisite.items():
                 if all([c[each] for each in v]):
-                    c[k]=True
-                    flag=True
+                    c[k] = True
+                    flag = True
                     used_k.append(k)
             for k in used_k:
                 del prerequisite[k]
-                
+
         return all(c)
