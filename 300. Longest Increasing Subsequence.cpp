fc2056e42ab4786c78c8a63054a3027d073f60a1
thekingofcity <wzl7777777@gmail.com>
2020-07-28 13:40:58 +0800

    300 revisit

--- 300. Longest Increasing Subsequence.cpp
+++ 300. Longest Increasing Subsequence.cpp
@@ -25,3 +25,34 @@ public:
 [4,3,2,1]
 [11,15,3,7,5,8,1]
 */
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 1);
+        int globalMax=1;
+        for(int i=1;i<nums.size();++i){
+            int m=1;
+            for(int j=0;j<i;++j)
+                if(nums[i]>nums[j])
+                    m=max(m, dp[j]+1);
+            dp[i]=m;
+            globalMax=max(m, globalMax);
+        }
+        return globalMax;
+    }
+};
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(), 0);
+        int len=0;
+        for(int i=0;i<nums.size();++i){
+            auto index=lower_bound(dp.begin(), dp.begin()+len, nums[i]);
+            if(index==dp.begin()+len)
+                ++len;
+            *index=nums[i];
+        }
+        return len;
+    }
+};
