bf74b9d2a054b0c4987c6f92b71833ee487e5d8e
thekingofcity <wzl7777777@gmail.com>
2020-07-28 13:10:40 +0800

    91 revisit

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -10,9 +10,26 @@ public:
             int tmp = (int)atoi(s.substr(i - 1, 2).c_str());
             if (tmp <= 26 && tmp >= 10)
                 dp[i + 1] += dp[i - 1];
-            if (tmp%10 != 0)//s[i]==0
+            if (tmp%10 != 0)  // s[i]!=0
                 dp[i + 1] += dp[i];
         }
         return dp[l];
+    }
+};
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.size()==0||s[0]=='0') return 0;
+        vector<int> dp(s.size()+1, 0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=1;i<s.size();++i){
+            if(s[i-1]=='1'||(s[i-1]=='2'&&s[i]<='6'))
+                dp[i+1]+=dp[i-1];
+            if(s[i]!='0')
+                dp[i+1]+=dp[i];
+        }
+        return dp[s.size()];
     }
 };
