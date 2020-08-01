9933368f3a9e84944631a0f10e066748d003eebe
thekingofcity <wzl7777777@gmail.com>
2020-08-01 16:57:48 +0800

    56 revisit

--- 56. Merge Intervals.cpp
+++ 56. Merge Intervals.cpp
@@ -35,3 +35,20 @@ public:
         return a.start<b.start;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        if(intervals.empty()) return res;
+        res.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();++i){
+            if(intervals[i][0]<=res.back()[1])
+                res.back()[1]=max(res.back()[1], intervals[i][1]);
+            else
+                res.push_back(intervals[i]);
+        }
+        return res;
+    }
+};
