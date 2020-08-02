885171dee639acf79392e943dbb67c2b0c257013
thekingofcity <wzl7777777@gmail.com>
2020-08-02 14:53:15 +0800

    91 revisit

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -33,3 +33,18 @@ public:
         return dp[s.size()];
     }
 };
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s.empty()) return 0;
+        vector<int> dp(s.size()+1, 0);
+        dp[0]=1;
+        for(int i=1;i<dp.size();++i){
+            dp[i]=s[i-1]=='0'?0:dp[i-1];
+            if(i>=2&&(s[i-2]=='1'||(s[i-2]=='2'&&s[i-1]<='6')))
+                dp[i]+=dp[i-2];
+        }
+        return dp.back();
+    }
+};
