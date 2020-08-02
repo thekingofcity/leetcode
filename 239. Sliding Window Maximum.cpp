700534241fd5aa0a8c27b2d2a091be1d469e3c37
thekingofcity <wzl7777777@gmail.com>
2020-08-02 16:17:48 +0800

    239 revisit

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -14,3 +14,31 @@ public:
         return ret;
     }
 };
+
+// 剑指offer 面试题59
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> q;
+        vector<int> ret;
+        q.push_back(0);
+        if(k==1) ret.push_back(nums[0]);
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]>=nums[q.front()]){
+                while(q.size()&&nums[i]>=nums[q.front()])
+                    q.pop_front();
+                q.push_front(i);
+            }else{
+                while(q.size()&&nums[i]>=nums[q.back()])
+                    q.pop_back();
+                q.push_back(i);
+            }
+            while(q.size()&&i-q.front()>=k)
+                q.pop_front();
+            // for(auto x:q) cout<<x<<' '; cout<<endl;
+            if(i>=k-1&&q.size())
+                ret.push_back(nums[q.front()]);
+        }
+        return ret;
+    }
+};
