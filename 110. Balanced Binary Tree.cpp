c0e5817744722924dad80e92c688ccab7a4f83d0
thekingofcity <wzl7777777@gmail.com>
2020-08-02 20:15:12 +0800

    110 revisit

--- 110. Balanced Binary Tree.cpp
+++ 110. Balanced Binary Tree.cpp
@@ -29,3 +29,31 @@ public:
 [1,2,2,3,3,null,null,4,4]
 [1,2,2,3,null,null,3,4,null,null,4]
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(helper(root)==-1) return false;
+        return true;
+    }
+    
+    int helper(TreeNode* root){
+        if(!root) return 0;
+        int left=helper(root->left);
+        int right=helper(root->right);
+        if(left<0||right<0) return -1;
+        if(abs(left-right)>1) return -1;
+        return max(left, right)+1;
+    }
+};
