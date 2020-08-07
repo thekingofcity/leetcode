350caae163fb64e7ba5a9223475fe71d0673b271
thekingofcity <wzl7777777@gmail.com>
2020-08-07 13:42:56 +0800

    141 revisit

--- 141. Linked List Cycle.cpp
+++ 141. Linked List Cycle.cpp
@@ -40,3 +40,25 @@ public:
         return false;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head) return false;
+        ListNode *fast=head,*slow=head;
+        while(fast->next&&fast->next->next&&slow->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow) return true;
+        }
+        return false;
+    }
+};
