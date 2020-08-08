8e82c540bd8bce9d6437f99832bf75c65e613edc
thekingofcity <wzl7777777@gmail.com>
2020-08-08 19:59:48 +0800

    169 revisit

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -22,3 +22,35 @@ public:
         return ret;
     }
 };
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        for(auto num:nums){
+            ++m[num];
+            if(m[num]>(nums.size()>>1))
+                return num;
+        }
+        return nums[0];
+    }
+};
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=1,major=nums[0];
+        for(int i=1;i<nums.size();++i){
+            if(major!=nums[i]){
+                --count;
+                if(count==0){
+                    count=1;
+                    major=nums[i];
+                }
+            }
+            else
+                ++count;
+        }
+        return major;
+    }
+};
