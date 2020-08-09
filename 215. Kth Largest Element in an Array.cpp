482431d2595619e60568c84d20ed490b8071ac0b
thekingofcity <wzl7777777@gmail.com>
2020-08-09 20:36:31 +0800

    215 revisit

--- 215. Kth Largest Element in an Array.cpp
+++ 215. Kth Largest Element in an Array.cpp
@@ -38,3 +38,55 @@ public:
         nums[b]=temp;
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for(int i=0;i<k-1;++i)
+            pq.pop();
+        return pq.top();
+    }
+};
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int index=-1, left=0, right=nums.size()-1;
+        --k;
+        while(true){
+            index=partition(nums, left, right);
+            // cout<<index<<' '<<left<<' '<<right<<endl;
+            // for(auto num:nums) cout<<num<<' '; cout<<endl;
+            if(k==index) return nums[index];
+            else if(index<k) left=index+1;
+            else right=index-1;
+        }
+        return -1;
+    }
+    
+    int partition(vector<int>& nums, int left, int right){
+        int l=left+1,r=right;
+        int pivot=nums[left];
+        while(l<=r){
+            if(pivot>nums[l]&&pivot<nums[r])
+                swap(nums[l++], nums[r--]);
+            if(pivot<=nums[l]) ++l;
+            if(pivot>=nums[r]) --r;
+        }
+        swap(nums[left], nums[r]);
+        return r;
+    }
+    int partitionAsc(vector<int>& nums, int left, int right){
+        if(left>=right) return left;
+        int pivot=left;
+        for(int i=pivot+1;i<=right;++i){
+            if(nums[i]>=nums[pivot])
+                swap(nums[i], nums[right--]);
+            else
+                ++pivot;
+        }
+        swap(nums[left], nums[pivot]);
+        return pivot;
+    }
+};
