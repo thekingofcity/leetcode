419011b637b5a3ffbb8b977667c777abb525326f
thekingofcity <wzl7777777@gmail.com>
2020-08-16 22:00:49 +0800

    81 revisit

--- 81. Search in Rotated Sorted Array II.cpp
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -24,3 +24,34 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1,mid=0;
+        while(l<=r){
+            mid=(l+r)/2;
+            if(nums[mid]==target) return true;
+
+            // search in linear time if l==mid==r
+            if(nums[l]==nums[mid]&&nums[mid]==nums[r]){
+                for(int i=l;i<=r;++i)
+                    if(nums[i]==target)
+                        return true;
+                return false;
+            }
+
+            if(nums[l]<=nums[mid])
+                if(nums[l]<=target&&target<=nums[mid])
+                    r=mid-1;
+                else
+                    l=mid+1;
+            else
+                if(nums[mid]<=target&&target<=nums[r])
+                    l=mid+1;
+                else
+                    r=mid-1;
+        }
+        return false;
+    }
+};
