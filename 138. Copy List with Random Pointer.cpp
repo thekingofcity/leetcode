d3c13151dc2291c4638adf36ca5e97bde108bdd1
thekingofcity <wzl7777777@gmail.com>
2020-12-09 21:02:27 +0800

    138 revisit

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -33,3 +33,48 @@ public:
         return ret;
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(nullptr==head) return nullptr;
+        Node* cur=head;
+        while(cur){
+            Node* curCopy=new Node(cur->val);
+            curCopy->next=cur->next;
+            cur->next=curCopy;
+            cur=cur->next->next;
+        }
+        cur=head;
+        while(cur){
+            if(cur->random){
+                cur->next->random=cur->random->next;
+            }
+            cur=cur->next->next;
+        }
+        cur=head;
+        Node* ret=cur->next,*curRet=ret;
+        while(cur){
+            Node* tmp=cur->next;
+            cur->next=tmp->next;
+            if(tmp->next) tmp->next=tmp->next->next;
+            cur=cur->next;
+        }
+        return ret;
+    }
+};
